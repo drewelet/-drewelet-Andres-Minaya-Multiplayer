52c5243575b59c2297ccd32f19dc76389c6df157
drewelet <64397513+drewelet@users.noreply.github.com>
2021-12-22 20:48:51 -0400

    Updated movemet script to a more robust one (Unity Example)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,14 +3,37 @@ using Mirror;
 
 public class PlayerMovement : NetworkBehaviour
 {
-    public float playerSpeed = 1.0f;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
 
+    private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+    }
     void Update()
     {
         if (isLocalPlayer)
         {
-            Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * playerSpeed, 0, Input.GetAxis("Vertical") * playerSpeed);
-            transform.position = transform.position + movement;
+            groundedPlayer = controller.isGrounded;
+            if (groundedPlayer && playerVelocity.y < 0)
+            {
+                playerVelocity.y = 0f;
+            }
+
+            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            controller.Move(move * Time.deltaTime * playerSpeed);
+
+            if (move != Vector3.zero)
+            {
+                gameObject.transform.forward = move;
+            }
+
+            playerVelocity.y += gravityValue * Time.deltaTime;
+            controller.Move(playerVelocity * Time.deltaTime);
         }
     }
 }
